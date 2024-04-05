419694fb2252979a056062521228158b020a5cce
Imran Rafiq Rather <emmeiwhite@gmail.com>
2024-04-05 14:48:08 +0530

    Every value has a type

--- index.ts
+++ index.ts
@@ -2,22 +2,27 @@ import axios from "axios";
 
 const apiUrl = "https://jsonplaceholder.typicode.com/todos/1";
 
-// Interfaces in TypeScript are used to structure our data
 interface Todo {
   id: number;
-  completed: boolean;
   title: string;
+  completed: boolean;
 }
 
 axios.get(apiUrl).then((response) => {
-  const { id, completed, title } = response.data as Todo;
+  // let's try catching some errors
+  const todo = response.data as Todo;
+
+  const id = todo.id;
+  const title = todo.title;
+  const completed = todo.completed;
+
   logTodo(id, completed, title);
 });
 
-const logTodo = (id: number, completed: boolean, title: string) => {
+function logTodo(id: number, completed: boolean, title: string) {
   console.log(`
-      ID is ${id},
-      Title is ${title},
-      Is it finished? ${completed}
-    `);
-};
+    The Todo with ID: ${id}
+    Has a title of: ${title}
+    Is it finished? ${completed}
+  `);
+}
